086033afb05a81e812964738ab81783e7f400b2d
Bekama Regasa Negewo <bekamregasa@gmail.com>
2026-09-14 16:07:47 +0300

    Update app.py port binding for Render

--- app.py
+++ app.py
@@ -1,22 +1,25 @@
+import os
 import gradio as gr
-from fastai.vision.all import load_learner, PILImage
+from fastai.vision.all import *
 
 learn = load_learner('model.pkl')
+labels = learn.dls.vocab
 
 def predict(img):
-    if img is None:
-        return {}
-    fastai_img = PILImage.create(img)
-    pred, pred_idx, probs = learn.predict(fastai_img)
-    labels = learn.dls.vocab
+    img = PILImage.create(img)
+    pred, pred_idx, probs = learn.predict(img)
     return {labels[i]: float(probs[i]) for i in range(len(labels))}
 
+image = gr.Image(type="pil")
+label = gr.Label(num_top_classes=2)
+
 demo = gr.Interface(
-    fn=predict,
-    inputs=gr.Image(type="pil"),
-    outputs=gr.Label(num_top_classes=2),
-    title="MNIST Digit Classifier",
-    description="Upload a handwritten digit image for real-time deep learning inference."
+    fn=predict, 
+    inputs=image, 
+    outputs=label,
+    title="MNIST Digit Classifier"
 )
 
-demo.launch(server_name="0.0.0.0", server_port=7860)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    demo.launch(server_name="0.0.0.0", server_port=port)
